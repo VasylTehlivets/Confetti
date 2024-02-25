eec20ad3137562b16eca9b8a6a6201810458df5a
VasylTehlivets <vasyltehlivets@gmail.com>
2024-02-25 22:51:38 +0200

    Update Cnt.js

--- src/components/Contacts/Cnt.js
+++ src/components/Contacts/Cnt.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import "./Contacts.css";
-
-// const Contacts = () => {
-//   return (
-//     <div id="contact">
-//       <h2 className="">Contacts</h2>
-//       <p>Confetti - Studio </p>
-//       <p>Dekoracij Balonami</p>
-//       <p>Warszawa </p>
-//       <div className="Phone">+48 793 351 407 </div>
-//       <div className="Email">confettibalony@gmail.com</div>
-//     </div>
-//   );
-// };
-
-// export default Contacts;
-
 import React, { useState } from "react";
 import "./Contacts.css";
 
