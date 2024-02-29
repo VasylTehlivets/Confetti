5bc5c056c9d6514946b495f0ce46f1e29cb629a3
VasylTehlivets <vasyltehlivets@gmail.com>
2024-02-29 20:50:38 +0200

    Create Ctn.js

---
+++ src/components/Contacts/Ctn.js
@@ -0,0 +1,17 @@
+// import React from "react";
+// import "./Contacts.css";
+
+// const Contacts = () => {
+//   return (
+//     <div id="contact">
+//       <h2 className="">Contacts</h2>
+//       <p>Confetti - Studio </p>
+//       <p>Dekoracij Balonami</p>
+//       <p>Warszawa </p>
+//       <div className="Phone">+48 793 351 407 </div>
+//       <div className="Email">confettibalony@gmail.com</div>
+//     </div>
+//   );
+// };
+
+// export default Contacts;
