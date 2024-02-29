ff2e8f5138bbfd1013de703a1ca1a5783df0cfb9
VasylTehlivets <vasyltehlivets@gmail.com>
2024-02-29 20:52:04 +0200

    Update Ctn.js

--- src/components/Contacts/Ctn.js
+++ src/components/Contacts/Ctn.js
@@ -3,25 +3,6 @@ import React, { useState } from "react";
 import "./Contacts.css";
 
 const Contacts = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [formErrors, setFormErrors] = useState({
-    nameError: "",
-    emailError: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     // Валідація полів "Ім'я" і "Електронна пошта"
