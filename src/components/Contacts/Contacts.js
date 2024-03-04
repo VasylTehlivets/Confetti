bca5091ec62756dd24a85750ea92d8eb7cc85390
VasylTehlivets <vasyltehlivets@gmail.com>
2024-03-04 23:22:14 +0200

    Update Contacts.js

--- src/components/Contacts/Contacts.js
+++ src/components/Contacts/Contacts.js
@@ -1,20 +1,20 @@
-import React from "react";
-import "./Contacts.css";
+// import React from "react";
+// import "./Contacts.css";
 
-const Contacts = () => {
-  return (
-    <div id="contact">
-      <h2 className="">Contacts</h2>
-      <p>Confetti - Studio </p>
-      <p>Dekoracij Balonami</p>
-      <p>Warszawa </p>
-      <div className="Phone">+48 793 351 407 </div>
-      <div className="Email">confettibalony@gmail.com</div>
-    </div>
-  );
-};
+// const Contacts = () => {
+//   return (
+//     <div id="contact">
+//       <h2 className="">Contacts</h2>
+//       <p>Confetti - Studio </p>
+//       <p>Dekoracij Balonami</p>
+//       <p>Warszawa </p>
+//       <div className="Phone">+48 793 351 407 </div>
+//       <div className="Email">confettibalony@gmail.com</div>
+//     </div>
+//   );
+// };
 
-export default Contacts;
+// export default Contacts;
 
 // import React, { useState } from "react";
 // import "./Contacts.css";
