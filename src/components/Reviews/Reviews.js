a9cf53b7c84e2603aac8351bc8b6da4eb6c67263
VasylTehlivets <vasyltehlivets@gmail.com>
2024-03-15 21:56:43 +0200

    Update Reviews.js

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,32 +1,3 @@
-// import React from "react";
-// import "./Reviews.css";
-// import user from "../../assets/tag-user.svg";
-// import Slider from "../Slider/Slider";
-
-// const Reviews = () => {
-//   return (
-//     <div id="review" className="">
-//       <h2 className="name">Recenzje</h2>
-//       <Slider />
-//       <div className="review">
-//         <p className="reviewDescr">
-//           Jeszcze raz wielkie dzięki! <br /> Ciężko nam było foty robić jak
-//           <br />
-//           ustawili te stoły 🥰 ale sam <br />
-//           klimat i dekoracja robiła <br /> meegaaa wrażenie!
-//         </p>
-//         <div className="tagUser">
-//           <img src={user} alt="tag-user" />
-//           <h3 className="reviewName">Aleksandra Murawska</h3>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Reviews;
-/////////////////////////////////////////////////////////////////////////////////
-
 // import React, { useState } from "react";
 // import "./Reviews.css";
 // import user1 from "../../assets/tag-user.svg";
